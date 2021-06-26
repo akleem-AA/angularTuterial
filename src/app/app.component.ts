974ea5978ee6d0fd9abdc98405efbaebb2e409ea
akleem <akleem789@gmail.com>
2021-06-26 16:55:17 +0530

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{UserDataService} from './user-data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LeanAngular';
+  name='Akleem ';
+  // lastName='khan';
+  // fistFun(){
+  //   return 'more lean then more earn';
+  // }
+  // secondFun(){
+  //   return this.lastName;
+  // }
+  data=[];
+  constructor(private user:UserDataService){
+    this.user.getData().subscribe(data=>{
+      console.warn(data)
+      this.data=data
+    })
+  }
+  
 }
