d5f16a0c2cc857f069b7535c4e33b7907dd84ac0
akleem <akleem789@gmail.com>
2021-08-17 18:37:18 +0530

    forLoop tuterail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,28 @@ export class AppComponent {
   title = 'LeanAngular';
   name='Akleem ';
   show='yes';
+  color='red'
+  data=['akleem','tushar','omkar','shivani']
+  data1=[{
+    name:'akleem',
+    age:22,
+    email:"akleem789@gami.com"
+  },
+{
+  name:'omkar',
+  age:34,
+  email:"akleem789@gami.com"
+},
+{
+name:'tushar',
+age:25,
+email:"akleem789@gami.com"
+},
+{
+  name:'shivani',
+  age:19,
+  email:"akleem789@gami.com"
+}]
   // lastName='khan';
   // fistFun(){
   //   return 'more lean then more earn';
